7ddb322068c2eabf8202ddcc448b9a7b72e8b567
debarati <deba@gmail.com>
2023-05-10 20:26:09 +0530

    for loop modified

--- forLoop.java
+++ forLoop.java
@@ -4,7 +4,7 @@ public class forLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int n = 1;n<=10;n++){
+		for (int n = 1;n<=15;n++){
 			System.out.println(n);
 			
 		}
