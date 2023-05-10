c91ea7ae3b0a1e86c62550fae3b438df9b3b7bfb
debarati <deba@gmail.com>
2023-05-10 20:16:52 +0530

    For loop prog added

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int n = 1;n<=10;n++){
+			System.out.println(n);
+			
+		}
+	}
+
+}
